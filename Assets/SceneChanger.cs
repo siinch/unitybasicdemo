3c6e83f3d8e477aa60b1c5d313ae2105f0c4e7e9
siinch <simon.ingemanns@protonmail.com>
2020-01-09 08:14:12 +0100

    changescene

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+          SceneManager.LoadScene(0);
+          Debug.Log("load scene");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+          SceneManager.LoadScene(1);
+          Debug.Log("load scene");
+        }
+    }
+}
